75295179aba1a4266613d417993c469efaf64510
Jorge Mendizabal <jorgemendiza@me.com>
2021-12-22 10:33:56 -0600

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jorge Mendizabal on 22/12/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
